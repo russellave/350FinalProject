2db578133e9ba9dde24eebb324c8bf312444521e
Russell Llave <ral30@duke.edu>
2019-04-20 02:01:43 -0400

    things light up

--- vga_controller.v
+++ vga_controller.v
@@ -63,11 +63,11 @@ end
 assign VGA_CLK_n = ~iVGA_CLK;
 
 //DEFINE ALL MY MIF FILES HERE
-//img_data	img_data_inst (
-//	.address ( ADDR ),
-//	.clock ( VGA_CLK_n ),
-//	.q ( index )
-//	);
+img_data	img_data_inst (
+	.address ( ADDR ),
+	.clock ( VGA_CLK_n ),
+	.q ( index )
+	);
 //	
 //splash_data	splash_data_inst (
 //	.address ( ADDR ),
@@ -83,13 +83,12 @@ nor norSplash(isSplash, screen[0], screen[1], screen[2], screen[3], screen[4], s
 //assign index = isSplash ? index_splash: index_main; 
 
 
-//////Color table output
-//img_index	img_index_inst (
-//	.address ( index ),
-//	.clock ( iVGA_CLK ),
-//	.q ( bgr_data_raw)
-//	);	
-assign bgr_data_raw = 24'd0; 
+////Color table output
+img_index	img_index_inst (
+	.address ( index ),
+	.clock ( iVGA_CLK ),
+	.q ( bgr_data_raw)
+	);	
 //////
 //////latch valid data at falling edge;
 
@@ -114,52 +113,54 @@ reg [7:0] score;
 reg [31:0] screen_reg; 
 reg [31:0] adjusted_sensor_out; 
 //   
-always@(posedge VGA_CLK_n) 
-begin
-	bgr_data <= bgr_data_raw;
-	sensor_in <= sensor_input; 
-	sensor_out <= sensor_output; 
-	controller_reg <= controller; 
 
 
-	
-	
-//	if((controller_reg > 32'd0) && (screen_reg > 32'd0)) 
+//always@(posedge VGA_CLK_n) 
+//begin
+//	bgr_data <= bgr_data_raw;
+//	sensor_in <= sensor_input; 
+//	sensor_out <= sensor_output; 
+//	controller_reg <= controller; 
+//
+//
+//	
+//	
+//	if((screen_reg == 32'd0)) 
 //	begin screen_reg <= 32'd1;
 //	end
+////	
 //	
-	
-	//get x and y locations of address
-	x <= ADDR % 10'd640;
-   y <= ADDR / 10'd640;
-	
+//	//get x and y locations of address
+//	x <= ADDR % 10'd640;
+//   y <= ADDR / 10'd640;
+//	
 	//GREEN IF HIT TARGET 
 	//pad 1: 154-193; 198-247
 	//pad 2: 296-336; 187-237
 	//pad 3: 447-489; 198-248
-//	if((screen_reg != 32'd0))
-//	begin
-		if((sensor_in[6:0] < 7'd40)&& (sensor_in[6:0] >7'd0) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h006400; 
-
-		else if((sensor_in[6:0] < 7'd80)&& (sensor_in[6:0] >7'd40) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h32CD32; 
-
-		else if((sensor_in[6:0] < 7'd120)&& (sensor_in[6:0] >7'd80) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h90EE90;
-
-		else if((sensor_in[13:7] < 7'd40)&& (sensor_in[13:7] >7'd0) && (x > 10'd296) && (x<10'd336) && (y<10'd237) && (y>10'd187)) color_output <= 24'h006400; 
-
-		else if((sensor_in[13:7] < 7'd80)&& (sensor_in[13:7] >7'd40) && (x > 10'd296) && (x<10'd336) && (y<10'd237) && (y>10'd187)) color_output <= 24'h32CD32; 
-
-		else if((sensor_in[13:7] < 7'd120)&& (sensor_in[13:7] >7'd80) && (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h90EE90;
-		
-		else if((sensor_in[20:14] < 7'd40)&& (sensor_in[20:14] >7'd0) && (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h006400; 
-
-		else if((sensor_in[20:14] < 7'd80)&& (sensor_in[20:14] >7'd40) && (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h32CD32; 
-
-		else if((sensor_in[20:14] < 7'd120)&& (sensor_in[20:14] >7'd80) && (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h90EE90;
-		else 
-		color_output <=bgr_data; 
-//	end
-//	
+	//if((screen_reg != 32'd0))
+	//begin
+//		if((sensor_in[6:0] < 7'd40)&& (sensor_in[6:0] >7'd0) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h006400; 
+//
+//		else if((sensor_in[6:0] < 7'd80)&& (sensor_in[6:0] >7'd40) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h32CD32; 
+//
+//		else if((sensor_in[6:0] < 7'd120)&& (sensor_in[6:0] >7'd80) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h90EE90;
+//
+//		else if((sensor_in[13:7] < 7'd40)&& (sensor_in[13:7] >7'd0) && (x > 10'd296) && (x<10'd336) && (y<10'd237) && (y>10'd187)) color_output <= 24'h006400; 
+//
+//		else if((sensor_in[13:7] < 7'd80)&& (sensor_in[13:7] >7'd40) && (x > 10'd296) && (x<10'd336) && (y<10'd237) && (y>10'd187)) color_output <= 24'h32CD32; 
+//
+//		else if((sensor_in[13:7] < 7'd120)&& (sensor_in[13:7] >7'd80) && (x > 10'd296) && (x<10'd336) && (y<10'd237) && (y>10'd187)) color_output <= 24'h90EE90;
+//		
+//		else if((sensor_in[20:14] < 7'd40)&& (sensor_in[20:14] >7'd0) && (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h006400; 
+//
+//		else if((sensor_in[20:14] < 7'd80)&& (sensor_in[20:14] >7'd40) && (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h32CD32; 
+//
+//		else if((sensor_in[20:14] < 7'd120)&& (sensor_in[20:14] >7'd80) && (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h90EE90;
+//		else 
+//		color_output <=bgr_data; 
+	//end
+	
 //	if((screen_reg == 32'd1))
 //	begin
 //		if(controller_reg == 32'd2) screen_reg <= 2; 
@@ -175,8 +176,48 @@ begin
 //	begin
 //	end
 
+//end
+reg [15:0] counter_hit; 
+always@(posedge VGA_CLK_n) 
+begin
+	begin
+		bgr_data <= bgr_data_raw;
+		sensor_in <= sensor_input;
+		x <= ADDR % 10'd640;
+		y <= ADDR / 10'd640;
+		// dark green: 24'h006400
+		//light green: 24'h90EE90
+		//middle green: h32CD32
+//		if((sensor_in[0] ==1'b1) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h90EE90; 
+		if((sensor_in[1] == 1'b1) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h90EE90; 
+//		else if((sensor_in[2] == 1'b1) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h32CD32; 
+//		else if((sensor_in[3] == 1'b1) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h32CD32; 
+//		else if((sensor_in[5] == 1'b1) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h006400;  
+//		else if(((sensor_in[4] == 1'b1)) && (x > 10'd154) && (x<10'd193) && (y<10'd247) && (y>10'd198)) color_output <= 24'h006400;  
+
+//		else if(((sensor_in[7] == 1'b1)) &&  (x > 10'd296) && (x<10'd336) && (y<10'd237) && (y>10'd187)) color_output <= 24'h90EE90; 
+		else if(((sensor_in[8] == 1'b1)) &&  (x > 10'd296) && (x<10'd336) && (y<10'd237) && (y>10'd187)) color_output <= 24'h90EE90; 
+//		else if(((sensor_in[9] == 1'b1)) &&  (x > 10'd296) && (x<10'd336) && (y<10'd237) && (y>10'd187)) color_output <= 24'h32CD32; 
+//		else if(((sensor_in[10] == 1'b1)) &&  (x > 10'd296) && (x<10'd336) && (y<10'd237) && (y>10'd187)) color_output <= 24'h32CD32; 
+//		else if(((sensor_in[11] == 1'b1)) &&  (x > 10'd296) && (x<10'd336) && (y<10'd237) && (y>10'd187)) color_output <= 24'h006400; 
+//		else if(((sensor_in[12] == 1'b1)) &&  (x > 10'd296) && (x<10'd336) && (y<10'd237) && (y>10'd187)) color_output <= 24'h006400; 
+
+	
+//		else if(((sensor_in[14] == 1'b1)) &&  (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h90EE90;
+		else if(((sensor_in[15] == 1'b1)) &&  (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h90EE90;
+//		else if(((sensor_in[16] == 1'b1)) &&  (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h32CD32;
+//		else if(((sensor_in[17] == 1'b1)) &&  (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h32CD32;
+//		else if(((sensor_in[19] == 1'b1)) &&  (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h006400;
+//		else if(((sensor_in[18] == 1'b1)) &&  (x > 10'd447) && (x<10'd489) && (y<10'd247) && (y>10'd198)) color_output <= 24'h006400;
+		else color_output <=bgr_data; 
+		
+	end
+	
+	
+	
 
 end
+
 assign b_data = color_output[23:16];
 assign g_data = color_output[15:8];
 assign r_data = color_output[7:0]; 
